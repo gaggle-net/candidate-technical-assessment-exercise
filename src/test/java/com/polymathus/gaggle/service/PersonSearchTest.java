be6f7613116d48ea436fdb973d43f0c4774b629e
polymathus <tommy@polymathus.com>
2021-08-10 17:22:51 -0400

    empty Test class with testing notes

---
+++ src/test/java/com/polymathus/gaggle/service/PersonSearchTest.java
@@ -0,0 +1,18 @@
+package com.polymathus.gaggle.service;
+
+class PersonSearchTest {
+
+    /*
+        Things I want to test
+
+        --- from the requirements specifically:
+            - accepts a number; subsequently searches on Primary Key
+            - accepts a string; subsequently searches for person with all or part of that input
+
+
+
+     */
+
+
+
+}
