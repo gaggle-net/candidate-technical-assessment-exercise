f2a1d22198f2f495f4897faaf75af47558c2edf2
polymathus <tommy@polymathus.com>
2021-08-11 18:49:34 -0400

    removed hardcoded input; take input at runtime

--- src/main/java/com/polymathus/gaggle/Main.java
+++ src/main/java/com/polymathus/gaggle/Main.java
@@ -3,6 +3,8 @@ package com.polymathus.gaggle;
 import com.polymathus.gaggle.service.PersonSearch;
 import org.json.simple.JSONObject;
 
+import java.util.Scanner;
+
 /**
  *
  */
@@ -10,21 +12,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        JSONObject formInputAsJson = getSampleUserInputAsJson("Bru");     //use a Scanner and get this input dynamically at runtime
+        //JSONObject formInputAsJson = getSampleUserInputAsJson("Bru");     //use a Scanner and get this input dynamically at runtime
 
-        PersonSearch personSearchService = new PersonSearch();
-        JSONObject output = personSearchService.search(formInputAsJson);
+        System.out.println("Welcome to Your Search !");
+        System.out.println("Please enter a name, part of a name or primary key of the record you seek.");
+        System.out.print("What say you? ---> ");
 
-        System.out.println(output.toString());
-    }
-
-
-    //assuming the user is entering freeform in a single field
-    private static JSONObject getSampleUserInputAsJson(String userInput) {
+        Scanner scanner = new Scanner (System.in);
+        String userEntry = scanner.next().trim();
 
         JSONObject formInputJson = new JSONObject();
-        formInputJson.put("searchInput", userInput);
+        formInputJson.put("searchInput", userEntry);
 
-        return formInputJson;
+        PersonSearch personSearchService = new PersonSearch();
+        JSONObject output = personSearchService.search(formInputJson);
+
+        System.out.println(output.toString());
     }
 }
